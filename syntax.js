4bf38c048313fbeaeadf11ed7f17c82e092f33f2
elvisdouglas <ssemagandaelvis20@gmail.com>
2021-05-25 09:35:16 +0300

    first commit

---
+++ syntax.js
@@ -0,0 +1,7 @@
+//Variable naming
+// no variable name should start with any special characters or numbers
+//variable name has no space 
+// you name variables with special key words
+//example
+//1num = 20
+//var let = 50
