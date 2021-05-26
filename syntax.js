8a2eb8ea909e58cf6a8506a806e5f8f42804635c
elvisdouglas <ssemagandaelvis20@gmail.com>
2021-05-26 09:48:11 +0300

    javascript datatypes

--- syntax.js
+++ syntax.js
@@ -12,3 +12,42 @@
 
 //let ans = num2 + num3;
 //console.log(ans);
+
+//number
+var num = 100;
+console.log(typeof(num));
+
+//string
+var name = "Elvis";
+console.log(typeof(name));
+
+//number
+var num2 = 100.0;
+console.log(typeof(num2));
+
+//Boolean
+var bool = false;
+console.log(typeof(bool));
+
+//undefined
+var num3
+console.log(typeof(num3));
+
+//Object
+var obj = null;
+console.log(typeof(obj));
+
+//string
+var num4 = "100";
+console.log(typeof(num4));
+
+//program is a group of lines of code
+// script is a javascript file
+
+var myList = [2, 4, 5, 2, "100", "elvis", 20.8]; //Array is similar to an object
+console.log(myList);
+console.log(typeof(myList));
+
+console.log(myList[5]);
+
+
